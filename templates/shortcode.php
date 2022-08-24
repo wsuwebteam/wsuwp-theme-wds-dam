19938aea9143b26d245aaa7d6a43c24be7c4ba75
Danial Bleile <danbleile@gmail.com>
2022-08-24 12:32:15 -0700

    Enhancement | Shortcode | Add background

--- templates/shortcode.php
+++ templates/shortcode.php
@@ -1,13 +1,17 @@
-<figure class="wp-block-gallery columns-3">
-	<ul class="blocks-gallery-grid">
-		<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-		<li class="blocks-gallery-item">
-			<figure>
-				<a href="<?php echo esc_url( get_attachment_link() ); ?>">
-					<?php echo wp_kses_post( wp_get_attachment_image( get_the_ID(), 'large' ) ); ?>
-				</a>
-			</figure>
-		</li>
-		<?php endwhile; ?>
-	</ul>
-</figure>
+<section class="wsu-section  wsu-width--full wsu-color-background--gray-5">
+    <div class="wsu-section__inner wsu-width--content"> 
+		<figure class="wp-block-gallery columns-5">
+			<ul class="blocks-gallery-grid">
+				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+				<li class="blocks-gallery-item">
+					<figure>
+						<a href="<?php echo esc_url( get_attachment_link() ); ?>">
+							<?php echo wp_kses_post( wp_get_attachment_image( get_the_ID(), 'medium' ) ); ?>
+						</a>
+					</figure>
+				</li>
+				<?php endwhile; ?>
+			</ul>
+		</figure>
+    </div>
+</section>
